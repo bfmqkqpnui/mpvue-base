cbba77aaffe0162982dc108245606265eb81f92a
陆逸飞 <47877962@qq.com>
2019-12-24 17:35:16 +0800

    新增时间轴组件

---
+++ src/components/timeLine/timeLine.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- 时间轴 -->
+  <div class="listview-container">
+    <div class="playlog-item" v-for="(item, i) in list" :key="i">
+      <div class="dotline">
+        <!-- 竖线 -->
+        <div class="line" v-if="i != list.length - 1" :style="{background: backColor ? backColor : '#CFBC90'}"></div>
+        <!-- 圆点 -->
+        <div class="dot" :style="{background: backColor ? backColor : '#CFBC90'}"></div>
+        <!-- 时间戳 -->
+      </div>
+      <div class="content">
+        <div class="course">{{item.title}}</div>
+        <div class="chapter">{{item.value}}</div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+  export default {
+    name: 'timeline',
+    props: {
+      list: {
+        type: Array,
+        value: []
+      },
+      backColor: {
+        type: String,
+        value: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" src="./css/timeLine.scss" scoped></style>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,12 +1,47 @@
 <template>
   <div class="home">
     首页
+    <logistics :list="list" :backColor="backColor"></logistics>
   </div>
 </template>
 
 <script>
+import logistics from '../../components/timeLine/timeLine'
   export default {
-    name: 'home'
+    name: 'home',
+    data() {
+      return {
+        list: [{
+          index: 1,
+          title: '2019-12-12 12:12:14',
+          value: '就立即离开教室的附近上空的飞机久科夫斯基的付款就'
+        },
+        {
+          index: 2,
+          title: '2019-12-13 12:12:14',
+          value: '就立即离开教室的附近上空的飞机久科夫斯基的付款就'
+        },{
+          index: 3,
+          title: '2019-12-14 12:12:14',
+          value: '就立即离开教室的附近上空的飞机久科夫斯基的付款就'
+        },{
+          index: 4,
+          title: '2019-12-15 12:12:14',
+          value: '就立即离开教室的附近上空的飞机久科夫斯基的付款就'
+        },{
+          index: 5,
+          title: '2019-12-22 12:12:14',
+          value: '就立即离开教室的附近上空的飞机久科夫斯基的付款就'
+        }],
+        backColor: ''
+      }
+    },
+    props: {},
+    onLoad() {},
+    onShow() {},
+    components: {
+      logistics
+    }
   }
 </script>
 
